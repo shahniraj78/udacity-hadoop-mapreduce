68f1c99969ea1feb24290e43517886691ee5d870
shahniraj78 <shahniraj78@gmail.com>
2014-05-07 07:03:29 -0500

    Create questions_by_epoch_reducer.py

---
+++ questions_by_epoch_reducer.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+import sys
+import csv
+import re
+from datetime import datetime
+from datetime import timedelta
+
+reader = csv.reader(sys.__stdin__,delimiter="\t")
+
+questions_epoch_dict = dict()
+
+for data in reader:
+	if data:

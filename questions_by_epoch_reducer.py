f0ca451fb707e4c3136e20bfd24f2125a3b973b3
shahniraj78 <shahniraj78@gmail.com>
2014-05-07 19:36:09 -0500

    Update questions_by_epoch_reducer.py

--- questions_by_epoch_reducer.py
+++ questions_by_epoch_reducer.py
@@ -6,10 +6,22 @@ import re
 from datetime import datetime
 from datetime import timedelta
 
-reader = csv.reader(sys.__stdin__,delimiter="\t")
-
 questions_epoch_dict = dict()
 
-for data in reader:
+for line in sys.stdin:
+	data = line.strip().split("\t")
 	if data:
-		
+		if len(data) == 2:
+			tag_epoch_key, q_list = data 
+			if questions_epoch_dict.has_key(tag_epoch_key):
+				tag_epoch_qlist = questions_epoch_dict[tag_epoch_key]
+				if tag_epoch_key in tag_epoch_qlist:
+					tag_epoch_qlist.append(q_list)
+					questions_epoch_dict[tag_epoch_key] = tag_epoch_qlist
+			else:
+				questions_epoch_dict[tag_epoch_key] = q_list
+
+if questions_epoch_dict:
+	for tag_epoch_key,q_list in questions_epoch_dict.items():
+		tag,epoch = tag_epoch_key.split(":")
+		print "%s\t%s\t%s" %(tag,epoch,",".join(q_list))
